034e2f44f5dc24f8cc394fd20c491706e1a64550
Troy Webber <troywebb95@gmail.com>
2022-07-14 09:50:59 +0100

    v1

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import { Amplify } from "aws-amplify";
+import awsconfig from "../src/aws-exports";
+Amplify.configure(awsconfig);
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
