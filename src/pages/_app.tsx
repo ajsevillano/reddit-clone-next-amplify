0ffa8f5e512e8955ca263f71c54f0ffe628529e5
Antonio Sevillano <correosevi@gmail.com>
2022-07-14 13:22:33 +0100

    Install typescript packages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { Amplify } from 'aws-amplify';
+// import awsconfig from "../src/aws-exports";
+// Amplify.configure(awsconfig);
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
